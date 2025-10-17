0e50ac08de7b4043b9c3747fd87fc56f979dc74f
Daniel J. L. Beltran <danilidonbeltran@gmail.com>
2025-10-17 12:36:40 +0200

    instead of removing game from queue when scraped, just mark it as scraped

--- src/jobs/scrape-game.ts
+++ src/jobs/scrape-game.ts
@@ -3,14 +3,14 @@ import { connectDB } from "../config/database";
 import logger from "../config/logger";
 import {
 	getOneGameFromQueue,
-	removeGameFromQueue,
+	setGameInQueue,
 } from "../models/game-queue.model";
 import { saveScrapeData } from "../models/scrape.model";
 import { SCRAPE_SOURCES } from "../schemas/scrape.schema";
 import { ProtondbScraper } from "../services/scraping/ProtondbScraper";
 import type { Scraper } from "../services/scraping/Scraper";
-import { SteamdeckhqScraper } from "../services/scraping/SteamdeckhqScraper";
 import { SharedeckScraper } from "../services/scraping/SharedeckScraper";
+import { SteamdeckhqScraper } from "../services/scraping/SteamdeckhqScraper";
 
 dotenv.config();
 
@@ -28,9 +28,15 @@ async function run() {
 			process.exit(0);
 		}
 
-		logger.info(`Scraping game ${gameInQueue.game_id}...`);
-
 		if (gameInQueue.rescrape) {
+			logger.info(`Scraping game ${gameInQueue.game_id}...`);
+			
+			// Mark as scraped so is not picked again if job fails or there are multiple instances
+			const utcNow = new Date(Date.now());
+			await setGameInQueue(
+				{ game_id: gameInQueue?.game_id, rescrape: false, rescraped_at: utcNow },
+			);
+
 			await runScrapeProcess(
 				new ProtondbScraper(),
 				SCRAPE_SOURCES.PROTONDB,
@@ -53,11 +59,11 @@ async function run() {
 			);
 		}
 
-		await removeGameFromQueue(gameInQueue.game_id);
-
-		logger.info(`Job scrape-game completed in ${
-			(Date.now() - startTime) / 1000
-		} seconds.`);
+		logger.info(
+			`Job scrape-game completed in ${
+				(Date.now() - startTime) / 1000
+			} seconds.`,
+		);
 		process.exit(0);
 	} catch (error) {
 		logger.error("Error scraping game:", error);
@@ -83,6 +89,9 @@ async function runScrapeProcess(
 		logger.error(
 			`Error in scrape process for game ${gameId} from source ${source}:`,
 			error,
+		);
+		await setGameInQueue(
+			{ game_id: gameId, rescrape_failed: true },
 		);
 	}
 }
--- src/schemas/game-queue.schema.ts
+++ src/schemas/game-queue.schema.ts
@@ -6,9 +6,15 @@ export const gameQueueSchema = z.object({
 	queued_at: z.date(), // old createdAt
 	rescrape: z.boolean().optional(),
 	regenerate: z.boolean().optional(),
+	rescraped_at: z.date().nullable().optional(),
+	rescrape_failed: z.boolean().optional(),
+	regenerated_at: z.date().nullable().optional(),
+	regenerate_failed: z.boolean().optional(),
 });
 
-export const inputGameQueueSchema = gameQueueSchema.omit({ queued_at: true });
+export const inputGameQueueSchema = gameQueueSchema.omit({
+	queued_at: true,
+});
 
 export type GameQueue = z.infer<typeof gameQueueSchema>;
 export type InputGameQueue = z.infer<typeof inputGameQueueSchema>;
